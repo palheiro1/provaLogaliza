8156a1a94dd60e4bf2152b8481fe33e3e3fbd9ce
Ivan <ivichon@gmail.com>
2022-11-23 21:18:36 +0000

    Una comarca distinta cada dia

--- src/hooks/useTodays.ts
+++ src/hooks/useTodays.ts
@@ -1,119 +1,64 @@
 import { DateTime } from "luxon";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import seedrandom from "seedrandom";
-import { galicianComarcas, GalicianCountryCode } from "../domain/comarcas.position";
 import { Country } from "../domain/countries";
 import { countriesI, CountryCode } from "../domain/countries.position";
 import { Guess, loadAllGuesses, saveGuesses } from "../domain/guess";
-import { areas, bigEnoughCountriesWithImage, countriesWithImage, environment, smallCountryLimit } from './../environment';
+import { areas, bigEnoughCountriesWithImage, countriesWithImage, smallCountryLimit } from './../environment';
 
 const forcedCountries: Record<string, CountryCode> = {
-  "2022-02-02": "TD",
-  "2022-02-03": "PY",
-  "2022-03-21": "HM",
-  "2022-03-22": "MC",
-  "2022-03-23": "PR",
-  "2022-03-24": "MX",
-  "2022-03-25": "SE",
-  "2022-03-26": "VU",
-  "2022-03-27": "TF",
-  "2022-03-28": "AU",
-  "2022-03-29": "DE",
-  "2022-03-30": "GA",
-  "2022-03-31": "AI",
-  "2022-04-01": "NE",
-  "2022-04-02": "ET",
-  "2022-04-03": "TZ",
-  "2022-04-04": "LV",
-  "2022-04-05": "CN",
-  "2022-04-06": "SO",
-  "2022-04-07": "JP",
-  "2022-04-08": "BJ",
-  "2022-04-09": "PW",
-  "2022-04-10": "TW",
-  "2022-04-11": "BS",
-  "2022-04-12": "GA",
-  "2022-04-13": "KZ",
-  "2022-04-14": "PH",
-  "2022-04-15": "MR",
-  "2022-04-16": "AE",
-  "2022-04-17": "SI",
-  "2022-04-18": "LT",
-  "2022-04-19": "PT",
-  "2022-04-20": "LA",
-  "2022-04-21": "TR",
-  "2022-04-22": "CF",
-  "2022-04-23": "MC",
-  "2022-04-24": "UY",
-  "2022-04-25": "CR",
-  "2022-04-26": "MM",
-  "2022-04-27": "ZW",
-  "2022-04-28": "HR",
-  "2022-04-29": "NC",
-  "2022-04-30": "CO",
-  "2022-05-01": "CU",
-  "2022-05-02": "ES",
-  "2022-05-03": "TM",
-  "2022-05-04": "LI",
-  "2022-05-05": "MK",
-  "2022-05-06": "CY",
-  "2022-05-07": "SR",
-  "2022-05-08": "JM",
-  "2022-05-09": "LR",
-  "2022-05-10": "MZ",
-  "2022-05-11": "AL",
-  "2022-05-12": "UA",
-  "2022-05-13": "SL",
-  "2022-05-14": "CV",
-  "2022-05-15": "BO",
-  "2022-05-16": "AF",
-  "2022-05-17": "TT",
-  "2022-05-18": "FI",
-  "2022-05-19": "CZ",
-  "2022-05-20": "NP",
-  "2022-05-21": "SZ",
-  "2022-05-22": "AR",
-  "2022-05-23": "PL",
-  "2022-05-24": "TO",
-  "2022-05-25": "BF",
-  "2022-05-26": "EE",
-  "2022-05-27": "UG",
-  "2022-05-28": "BT",
-  "2022-05-29": "FJ",
-  "2022-05-30": "ER",
-  "2022-05-31": "PY",
-  "2022-06-01": "SH",
-  "2022-06-02": "US",
-  "2022-06-03": "BG",
-  "2022-06-04": "DJ",
-  "2022-06-05": "GM",
-  "2022-06-06": "KH",
-  "2022-06-07": "NA",
-  "2022-06-08": "UZ",
-  "2022-06-09": "BH",
-  "2022-06-10": "CG",
-  "2022-06-11": "NZ",
-  "2022-06-12": "DK",
-  "2022-06-13": "BD",
-  "2022-06-14": "ML",
-  "2022-06-15": "BW",
-  "2022-06-16": "NG",
-  "2022-06-17": "KY",
-  "2022-06-18": "AO",
-  "2022-06-19": "MA",
-  "2022-06-20": "KE",
-  "2022-06-21": "MN",
-  "2022-06-22": "PG",
-  "2022-06-23": "SN",
-  "2022-06-24": "AQ",
-  "2022-06-25": "GH",
-  "2022-06-26": "IS",
-  "2022-06-27": "MD",
-  "2022-06-28": "NO",
-  "2022-06-29": "SC",
-  "2022-06-30": "MD",
-  "2022-07-01": "VE",
-};
+  "2022-11-23": "TNA",
+  "2022-11-24": "MAR",
+  "2022-11-25": "BEZ",
+  "2022-11-26": "ORT",
+  "2022-11-27": "TTR",
+  "2022-11-28": "DEÇ",
+  "2022-11-29": "TAV",
+  "2022-11-30": "CAL",
+  "2022-12-01": "LIM",
+  "2022-12-02": "BER",
+  "2022-12-03": "MAS",
+  "2022-12-04": "BAR",
+  "2022-12-05": "TCA",
+  "2022-12-06": "ORD",
+  "2022-12-07": "ARN",
+  "2022-12-08": "MON",
+  "2022-12-09": "TMO",
+  "2022-12-10": "BUR",
+  "2022-12-11": "MUR",
+  "2022-12-12": "TME",
+  "2022-12-13": "VIA",
+  "2022-12-14": "BAM",
+  "2022-12-15": "CON",
+  "2022-12-16": "LUG",
+  "2022-12-17": "SAL",
+  "2022-12-18": "PAR",
+  "2022-12-19": "BAL",
+  "2022-12-20": "MOR",
+  "2022-12-21": "VDI",
+  "2022-12-22": "SEA",
+  "2022-12-23": "VIG",
+  "2022-12-24": "TRA",
+  "2022-12-25": "ANC",
+  "2022-12-26": "CAB",
+  "2022-12-27": "OUR",
+  "2022-12-28": "EUM",
+  "2022-12-29": "VDO",
+  "2022-12-30": "TCH",
+  "2022-12-31": "ULH",
+  "2023-01-01": "RIB",
+  "2023-01-02": "CHA",
+  "2023-01-03": "TLE",
+  "2023-01-04": "COR",
+  "2023-01-05": "ARÇ",
+  "2023-01-06": "CEL",
+  "2023-01-07": "COS",
+  "2023-01-08": "COU",
+  "2023-01-09": "SAR",
+  "2023-01-10": "COM",
+  "2023-01-11": "PON",
+  "2023-01-12": "CAR"
+}
 
 const noRepeatStartDate = DateTime.fromFormat("2022-05-01", "yyyy-MM-dd");
 
@@ -185,20 +130,13 @@ function getCountry(dayString: string) {
 
     const pickingDateString = pickingDate.toFormat("yyyy-MM-dd");
 
-    let forcedCountryCode: CountryCode | GalicianCountryCode;
-    
-    if (environment) {
-      forcedCountryCode = forcedCountries[dayString];
-    } else {
-      const random = Math.floor((Math.random() * 100)) % galicianComarcas.length;
-      forcedCountryCode = galicianComarcas[random].code;
-    }
+    const forcedCountryCode = forcedCountries[dayString];
 
     const forcedCountry =
       forcedCountryCode != null
         ? countriesWithImage.find(
           (country: countriesI) => country.code === forcedCountryCode
-          )
+        )
         : undefined;
 
     const countrySelection =
@@ -225,7 +163,7 @@ function getCountry(dayString: string) {
     if (areas[pickedCountry!.code] < smallCountryLimit) {
       smallCountryCooldown = 7;
     }
-    
+
 
     lastPickDates[pickedCountry!.code] = pickingDate;
     pickingDate = pickingDate.plus({ day: 1 });
